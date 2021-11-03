368a654cfce356359139c123829770f8ff71f2c1
lby9906 <lby9906@naver.com>
2021-11-03 17:48:33 +0900

    datalink 추가

--- main.js
+++ main.js
@@ -11,3 +11,27 @@ document.addEventListener("scroll", () => {
     }
 
 });
+
+// 넷바메뉴 선택시 해당 부분으로 이동하기
+const navbarMenu = document.querySelector (".navbar__menu");
+navbarMenu.addEventListener ("click", (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if  (link == null){
+        return;
+    }
+    console.log(link);
+    scrollIntoView(link);
+});
+
+//contact me 버튼 이동하기
+const contactMe = document.querySelector('.home__contact');
+contactMe.addEventListener ("click", () => {
+    scrollIntoView('#contact');
+});
+
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior : 'smooth'});
+}
